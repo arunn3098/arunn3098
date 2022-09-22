89fcbdc751499d46770a59fdc6a973fe6bfe2616
arunn3098 <arunbca3098>
2022-09-22 15:50:14 +0530

    Hai

---
+++ src/com/collection/List.java
@@ -0,0 +1,11 @@
+
+package com.collection;
+
+public interface List {
+	void add();
+	public static void main(String[] args) {
+		List l=new Listdemo();
+		l.add();
+	}
+
+}
---
+++ src/com/collection/Listdemo.java
@@ -0,0 +1,10 @@
+package com.collection;
+
+public class Listdemo implements List {
+	@Override
+	public void add() {
+System.out.println("demo");		
+	}
+	
+
+}
